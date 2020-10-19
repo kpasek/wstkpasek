0c82e2d16c53391aa9887f26763760ca5f9d46f9
Kamil Pasek <kamilpasek@gmail.com>
2020-10-19 11:22:56 +0200

    create sample components and setup routing

--- wstkpasek/ClientApp/src/App.js
+++ wstkpasek/ClientApp/src/App.js
@@ -3,6 +3,16 @@ import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
+import { Trainings } from './components/Trainings';
+import { Exercises } from './components/Exercises';
+import { Series } from './components/Series';
+import { Schedule } from './components/Schedule';
+import { STrainings } from './components/STrainings';
+import { SExercises } from './components/SExercises';
+import { SSeries } from './components/SSeries';
+import { RunTraining } from './components/RunTraining';
+import { User } from './components/User';
+import { Admin } from './components/Admin';
 import { Counter } from './components/Counter';
 
 import './custom.css'
@@ -14,8 +24,16 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Route path='/trening' component={Trainings} />
+        <Route path='/cwiczenia' component={Exercises} />
+        <Route path='/seria' component={Series} />
+        <Route path='/harmonogram' component={Schedule} />
+        <Route path='/harmonogram/trening' component={STrainings} />
+        <Route path='/harmonogram/cwiczenia' component={SExercises} />
+        <Route path='/harmonogram/seria' component={SSeries} />
+        <Route path='/harmonogram/trening/wykonaj' component={RunTraining} />
+        <Route path='/user/konto' component={User} />
+        <Route path='/admin' component={Admin} />
       </Layout>
     );
   }
