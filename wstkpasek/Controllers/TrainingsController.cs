12ba59b88c9c51ddc4e46cd969890c6878cb6130
Kamil Pasek <kamilpasek@gmail.com>
2020-10-20 15:26:28 +0200

    add comments to controller

--- wstkpasek/Controllers/TrainingsController.cs
+++ wstkpasek/Controllers/TrainingsController.cs
@@ -11,102 +11,125 @@ using wstkpasek.Models.TrainingModel;
 
 namespace wstkpasek.Controllers
 {
-  [Route("api/trainings")]
-  [ApiController]
-  [Authorize]
-  public class TrainingsController : ControllerBase
-  {
-    private readonly AppDBContext _context;
-
-    public TrainingsController(AppDBContext context)
+    [Route("api/trainings")]
+    [ApiController]
+    [Authorize]
+    public class TrainingsController : ControllerBase
     {
-      _context = context;
-    }
+        private readonly AppDBContext _context;
+        private readonly ITrainingRepository trainingRepository;
 
-    // GET: api/Trainings
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<Training>>> GetTrainings()
-    {
-      return await _context.Trainings.ToListAsync();
-    }
-
-    // GET: api/Trainings/5
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Training>> GetTraining(int id)
-    {
-      var training = await _context.Trainings.FindAsync(id);
-
-      if (training == null)
-      {
-        return NotFound();
-      }
-
-      return training;
-    }
-
-    // PUT: api/Trainings/5
-    // To protect from overposting attacks, enable the specific properties you want to bind to, for
-    // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-    [HttpPut("{id}")]
-    public async Task<IActionResult> PutTraining(int id, Training training)
-    {
-      if (id != training.TrainingId)
-      {
-        return BadRequest();
-      }
-
-      _context.Entry(training).State = EntityState.Modified;
-
-      try
-      {
-        await _context.SaveChangesAsync();
-      }
-      catch (DbUpdateConcurrencyException)
-      {
-        if (!TrainingExists(id))
+        public TrainingsController(AppDBContext context, ITrainingRepository trainingRepository)
         {
-          return NotFound();
+            _context = context;
+            this.trainingRepository = trainingRepository;
         }
-        else
+        /// <summary>
+        /// get email address from token
+        /// </summary>
+        /// <returns>user email from token</returns>
+        private string GetEmail()
         {
-          throw;
+            return this.User.Identity.Name;
         }
-      }
 
-      return NoContent();
+        // GET: api/Trainings
+        /// <summary>
+        /// use trainings repository to get all user trainings
+        /// </summary>
+        /// <returns>trainigs list in json format</returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Training>>> GetTrainings()
+        {
+            var email = GetEmail();
+            var userTrainings = await trainingRepository.GetTrainings(email);
+            return userTrainings;
+        }
+
+        // GET: api/Trainings/5
+        /// <summary>
+        /// Use repository to get one user training.
+        /// </summary>
+        /// <param name="id">IdTraining</param>
+        /// <returns>One uesr training</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Training>> GetTraining(int id)
+        {
+            var email = GetEmail();
+            var training = await trainingRepository.GetTraining(id, email);
+
+            if (training == null) return NotFound();
+
+            return training;
+        }
+
+        // PUT: api/Trainings/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        /// <summary>
+        /// update all fields in training. User need to be owner and training id must be the same as in route
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="training"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTraining(int id, Training training)
+        {
+            var email = GetEmail();
+            if (id != training.TrainingId || !trainingRepository.CheckTrainingOwner(id, email)) return BadRequest();
+            training.UserEmail = email;
+            _context.Entry(training).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TrainingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Trainings
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Training>> PostTraining(Training training)
+        {
+            _context.Trainings.Add(training);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTraining", new { id = training.TrainingId }, training);
+        }
+
+        // DELETE: api/Trainings/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Training>> DeleteTraining(int id)
+        {
+            var training = await _context.Trainings.FindAsync(id);
+            if (training == null)
+            {
+                return NotFound();
+            }
+
+            _context.Trainings.Remove(training);
+            await _context.SaveChangesAsync();
+
+            return training;
+        }
+
+        private bool TrainingExists(int id)
+        {
+            return _context.Trainings.Any(e => e.TrainingId == id);
+        }
     }
-
-    // POST: api/Trainings
-    // To protect from overposting attacks, enable the specific properties you want to bind to, for
-    // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-    [HttpPost]
-    public async Task<ActionResult<Training>> PostTraining(Training training)
-    {
-      _context.Trainings.Add(training);
-      await _context.SaveChangesAsync();
-
-      return CreatedAtAction("GetTraining", new { id = training.TrainingId }, training);
-    }
-
-    // DELETE: api/Trainings/5
-    [HttpDelete("{id}")]
-    public async Task<ActionResult<Training>> DeleteTraining(int id)
-    {
-      var training = await _context.Trainings.FindAsync(id);
-      if (training == null)
-      {
-        return NotFound();
-      }
-
-      _context.Trainings.Remove(training);
-      await _context.SaveChangesAsync();
-
-      return training;
-    }
-
-    private bool TrainingExists(int id)
-    {
-      return _context.Trainings.Any(e => e.TrainingId == id);
-    }
-  }
 }
