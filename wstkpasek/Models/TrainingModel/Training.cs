498f642bb23fe5a651d29755cff571e5413eada7
Kamil Pasek <kamilpasek@gmail.com>
2020-10-20 15:25:39 +0200

    fix format

--- wstkpasek/Models/TrainingModel/Training.cs
+++ wstkpasek/Models/TrainingModel/Training.cs
@@ -3,16 +3,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace wstkpasek.Models.TrainingModel
 {
-  [Table("trainings")]
-  public class Training
-  {
-    [Key]
-    public int TrainingId { get; set; }
-    public string UserEmail { get; set; }
-    // public DateTime DataUtworzenia { get; set; } = DateTime.Now;
-    // public DateTime Data { get; set; }
-    public string Name { get; set; }
-    public int ExerciseNumber { get; set; } = 2;
-    public bool Public { get; set; } = false;
-  }
+    [Table("trainings")]
+    public class Training
+    {
+        [Key]
+        public int TrainingId { get; set; }
+        public string UserEmail { get; set; }
+        // public DateTime DataUtworzenia { get; set; } = DateTime.Now;
+        // public DateTime Data { get; set; }
+        public string Name { get; set; }
+        public int ExerciseNumber { get; set; } = 2;
+        public bool Public { get; set; } = false;
+    }
 }
