e45c64d9fba11cf9942b30ab131143484d8a4ff1
Kamil Pasek <kamilpasek@gmail.com>
2020-10-23 21:57:03 +0200

    add training order

--- wstkpasek/Models/TrainingModel/TrainingRepository.cs
+++ wstkpasek/Models/TrainingModel/TrainingRepository.cs
@@ -8,152 +8,152 @@ using Microsoft.EntityFrameworkCore;
 
 namespace wstkpasek.Models.TrainingModel
 {
-  public class TrainingRepository : ITrainingRepository
-  {
+    public class TrainingRepository : ITrainingRepository
+    {
 
-    private readonly AppDBContext db;
+        private readonly AppDBContext db;
 
-    public TrainingRepository(AppDBContext db)
-    {
-      this.db = db;
-    }
-    /// <summary>
-    /// get one trainig by training id and check user is training owner
-    /// </summary>
-    /// <param name="trainingId">Training id</param>
-    /// <param name="email">user email from token</param>
-    /// <returns>one user training</returns>
-    public async Task<Training> GetTraining(int trainingId, string email)
-    {
-      var trainings = db.Trainings.Where(t => t.TrainingId == trainingId && t.UserEmail == email);
-      return await trainings.AnyAsync() ? await trainings.SingleAsync() : null;
-    }
-    /// <summary>
-    /// Download and return list of all user trainings
-    /// </summary>
-    /// <param name="email">user email from token</param>
-    /// <returns>List<Training></returns>
-    public async Task<List<Training>> GetTrainings(string email)
-    {
-      var trainings = db.Trainings.Where(t => t.UserEmail == email);
-      return await trainings.AnyAsync() ? await trainings.ToListAsync() : new List<Training>();
-    }
-    /// <summary>
-    /// return true when user is trainig owner
-    /// </summary>
-    /// <param name="trainingId">training id</param>
-    /// <param name="email">user email from token</param>
-    /// <returns>true or false</returns>
-    public bool CheckTrainingOwner(int trainingId, string email)
-    {
-      var t = db.Trainings.Where(tr => tr.TrainingId == trainingId && tr.UserEmail == email);
-      return t.Any();
-    }
-    /// <summary>
-    /// return list of all exercises assigned training
-    /// </summary>
-    /// <param name="trainingId">trainig id</param>
-    /// <param name="email">user email from token</param>
-    /// <returns>list of exercises</returns>
-    public async Task<List<Exercise>> GetExerciseInTrainingAsync(int trainingId, string email)
-    {
-      var trainingExercises = db.TrainingExercises
-        .Include(tc => tc.Exercise)
-        .Where(t => t.TrainingId == trainingId && t.UserEmail == email)
-        .OrderBy(t => t.Order);
-      if (!await trainingExercises.AnyAsync()) return null;
+        public TrainingRepository(AppDBContext db)
+        {
+            this.db = db;
+        }
+        /// <summary>
+        /// get one trainig by training id and check user is training owner
+        /// </summary>
+        /// <param name="trainingId">Training id</param>
+        /// <param name="email">user email from token</param>
+        /// <returns>one user training</returns>
+        public async Task<Training> GetTraining(int trainingId, string email)
+        {
+            var trainings = db.Trainings.Where(t => t.TrainingId == trainingId && t.UserEmail == email);
+            return await trainings.AnyAsync() ? await trainings.SingleAsync() : null;
+        }
+        /// <summary>
+        /// Download and return list of all user trainings
+        /// </summary>
+        /// <param name="email">user email from token</param>
+        /// <returns>List<Training></returns>
+        public async Task<List<Training>> GetTrainings(string email)
+        {
+            var trainings = db.Trainings.Where(t => t.UserEmail == email).OrderBy(o => o.TrainingId);
+            return await trainings.AnyAsync() ? await trainings.ToListAsync() : new List<Training>();
+        }
+        /// <summary>
+        /// return true when user is trainig owner
+        /// </summary>
+        /// <param name="trainingId">training id</param>
+        /// <param name="email">user email from token</param>
+        /// <returns>true or false</returns>
+        public bool CheckTrainingOwner(int trainingId, string email)
+        {
+            var t = db.Trainings.Where(tr => tr.TrainingId == trainingId && tr.UserEmail == email);
+            return t.Any();
+        }
+        /// <summary>
+        /// return list of all exercises assigned training
+        /// </summary>
+        /// <param name="trainingId">trainig id</param>
+        /// <param name="email">user email from token</param>
+        /// <returns>list of exercises</returns>
+        public async Task<List<Exercise>> GetExerciseInTrainingAsync(int trainingId, string email)
+        {
+            var trainingExercises = db.TrainingExercises
+              .Include(tc => tc.Exercise)
+              .Where(t => t.TrainingId == trainingId && t.UserEmail == email)
+              .OrderBy(t => t.Order);
+            if (!await trainingExercises.AnyAsync()) return null;
 
-      var list = await trainingExercises.ToListAsync();
-      return list.Select(cw => cw.Exercise).ToList();
-    }
+            var list = await trainingExercises.ToListAsync();
+            return list.Select(cw => cw.Exercise).ToList();
+        }
 
-    public async Task<Exercise> FindExercise(Exercise cw, string email)
-    {
-      var exercises = db.Exercises.Where(c => c.Name == cw.Name
-                                              && c.Description == cw.Description
-                                              && c.PartId == cw.PartId
-                                              && c.TypeId == cw.TypeId
-                                              && c.UserEmail == email);
+        public async Task<Exercise> FindExercise(Exercise cw, string email)
+        {
+            var exercises = db.Exercises.Where(c => c.Name == cw.Name
+                                                    && c.Description == cw.Description
+                                                    && c.PartId == cw.PartId
+                                                    && c.TypeId == cw.TypeId
+                                                    && c.UserEmail == email);
 
-      return exercises.Any() ? await exercises.SingleAsync() : null;
-    }
+            return exercises.Any() ? await exercises.SingleAsync() : null;
+        }
 
-    public async Task<TrainingExercise> GetTrainingExerciseAsync(int trainingId, int exerciseId, string email)
-    {
-      var trainingExercises = db.TrainingExercises
-        .Include(t => t.Training)
-        .Include(c => c.Exercise)
-        .Where(t => t.TrainingId == trainingId && t.ExerciseId == exerciseId && t.UserEmail == email);
-      return !trainingExercises.Any() ? null : await trainingExercises.SingleAsync();
-    }
+        public async Task<TrainingExercise> GetTrainingExerciseAsync(int trainingId, int exerciseId, string email)
+        {
+            var trainingExercises = db.TrainingExercises
+              .Include(t => t.Training)
+              .Include(c => c.Exercise)
+              .Where(t => t.TrainingId == trainingId && t.ExerciseId == exerciseId && t.UserEmail == email);
+            return !trainingExercises.Any() ? null : await trainingExercises.SingleAsync();
+        }
 
-    public async Task<TrainingExercise> GetTrainingExerciseByOrderAsync(int trainingId, int order, string email)
-    {
-      var trainingExercises = db.TrainingExercises
-        .Include(t => t.Training)
-        .Include(c => c.Exercise)
-        .Where(t => t.TrainingId == trainingId && t.Order == order && t.UserEmail == email);
-      return !trainingExercises.Any() ? null : await trainingExercises.SingleAsync();
-    }
+        public async Task<TrainingExercise> GetTrainingExerciseByOrderAsync(int trainingId, int order, string email)
+        {
+            var trainingExercises = db.TrainingExercises
+              .Include(t => t.Training)
+              .Include(c => c.Exercise)
+              .Where(t => t.TrainingId == trainingId && t.Order == order && t.UserEmail == email);
+            return !trainingExercises.Any() ? null : await trainingExercises.SingleAsync();
+        }
 
-    public async Task<int> GetMaxOrderInTrainingAsync(int trainingId, string email)
-    {
-      var trainingExercises = db.TrainingExercises.Where(t => t.TrainingId == trainingId && t.UserEmail == email);
-      return !await trainingExercises.AnyAsync() ? 0 : trainingExercises.Max(m => m.Order);
-    }
+        public async Task<int> GetMaxOrderInTrainingAsync(int trainingId, string email)
+        {
+            var trainingExercises = db.TrainingExercises.Where(t => t.TrainingId == trainingId && t.UserEmail == email);
+            return !await trainingExercises.AnyAsync() ? 0 : trainingExercises.Max(m => m.Order);
+        }
 
-    public async Task<List<Training>> GetTrainingByPartAndExerciseAsync(int exerciseId, string part, string email)
-    {
-      var tc = db.TrainingExercises
-        .Include(c => c.Exercise)
-        .ThenInclude(c => c.Part)
-        .Include(t => t.Training)
-        .Where(t => t.ExerciseId == exerciseId && t.UserEmail == email && t.Exercise.PartId == part);
-      if (!await tc.AnyAsync()) return null;
-      var lists = await tc.ToListAsync();
-      return lists.Select(tr => tr.Training).ToList();
-    }
+        public async Task<List<Training>> GetTrainingByPartAndExerciseAsync(int exerciseId, string part, string email)
+        {
+            var tc = db.TrainingExercises
+              .Include(c => c.Exercise)
+              .ThenInclude(c => c.Part)
+              .Include(t => t.Training)
+              .Where(t => t.ExerciseId == exerciseId && t.UserEmail == email && t.Exercise.PartId == part);
+            if (!await tc.AnyAsync()) return null;
+            var lists = await tc.ToListAsync();
+            return lists.Select(tr => tr.Training).ToList();
+        }
 
-    public async Task<List<Training>> GetTrainingByPartAsync(string part, string email)
-    {
-      var trainingExercises = db.TrainingExercises
-        .Include(c => c.Exercise)
-        .ThenInclude(p => p.Part)
-        .Include(t => t.Training)
-        .Distinct()
-        .Where(t => t.UserEmail == email && t.Exercise.PartId == part);
-      return !await trainingExercises.AnyAsync()
-        ? null
-        : trainingExercises.Select(trainingExercise => trainingExercise.Training).Distinct().ToList();
-    }
+        public async Task<List<Training>> GetTrainingByPartAsync(string part, string email)
+        {
+            var trainingExercises = db.TrainingExercises
+              .Include(c => c.Exercise)
+              .ThenInclude(p => p.Part)
+              .Include(t => t.Training)
+              .Distinct()
+              .Where(t => t.UserEmail == email && t.Exercise.PartId == part);
+            return !await trainingExercises.AnyAsync()
+              ? null
+              : trainingExercises.Select(trainingExercise => trainingExercise.Training).Distinct().ToList();
+        }
 
-    public async Task<List<Training>> GetTrainingByExerciseAsync(int exerciseId, string email)
-    {
-      var trainingExercises = db.TrainingExercises
-        .Include(t => t.Training)
-        .Where(t => t.ExerciseId == exerciseId && t.UserEmail == email);
-      if (!await trainingExercises.AnyAsync()) return null;
-      return await trainingExercises.Select(trainingExercise => trainingExercise.Training).ToListAsync();
-    }
-    public async Task<List<TrainingExercise>> GetExercisesTrainingsAsync(int trainingId, string email)
-    {
-      var trainingExercises = db.TrainingExercises.Include(c => c.Exercise)
-        .Where(t => t.TrainingId == trainingId && t.UserEmail == email);
-      return !await trainingExercises.AnyAsync() ? null : await trainingExercises.ToListAsync();
-    }
+        public async Task<List<Training>> GetTrainingByExerciseAsync(int exerciseId, string email)
+        {
+            var trainingExercises = db.TrainingExercises
+              .Include(t => t.Training)
+              .Where(t => t.ExerciseId == exerciseId && t.UserEmail == email);
+            if (!await trainingExercises.AnyAsync()) return null;
+            return await trainingExercises.Select(trainingExercise => trainingExercise.Training).ToListAsync();
+        }
+        public async Task<List<TrainingExercise>> GetExercisesTrainingsAsync(int trainingId, string email)
+        {
+            var trainingExercises = db.TrainingExercises.Include(c => c.Exercise)
+              .Where(t => t.TrainingId == trainingId && t.UserEmail == email);
+            return !await trainingExercises.AnyAsync() ? null : await trainingExercises.ToListAsync();
+        }
 
-    public async Task<List<Training>> GetPublicTrainingsAsync()
-    {
-      var trainings = db.Trainings.Where(t => t.Public);
-      return !await trainings.AnyAsync() ? null : await trainings.ToListAsync();
-    }
+        public async Task<List<Training>> GetPublicTrainingsAsync()
+        {
+            var trainings = db.Trainings.Where(t => t.Public);
+            return !await trainings.AnyAsync() ? null : await trainings.ToListAsync();
+        }
 
-    public async Task<List<Training>> GetTrainingsAdminAsync(string User, string Name)
-    {
-      var trainings = db.Trainings.Where(t => EF.Functions.ILike(t.UserEmail, $"%{User}%")
-                                              && EF.Functions.ILike(t.Name, $"%{Name}%"))
-                                  .Take(50);
-      return await trainings.AnyAsync() ? await trainings.ToListAsync() : null;
+        public async Task<List<Training>> GetTrainingsAdminAsync(string User, string Name)
+        {
+            var trainings = db.Trainings.Where(t => EF.Functions.ILike(t.UserEmail, $"%{User}%")
+                                                    && EF.Functions.ILike(t.Name, $"%{Name}%"))
+                                        .Take(50);
+            return await trainings.AnyAsync() ? await trainings.ToListAsync() : null;
+        }
     }
-  }
 }
