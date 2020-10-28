8b4ce2a38c11f9cd8718fa75df9b024aec36146a
Kamil Pasek <kamilpasek@gmail.com>
2020-10-28 17:36:44 +0100

    fix auth check & routing

--- wstkpasek/ClientApp/src/App.jsx
+++ wstkpasek/ClientApp/src/App.jsx
@@ -7,8 +7,6 @@ import { Exercises } from "./components/Exercises/Exercises";
 import { Series } from "./components/Series";
 import { Schedule } from "./components/Schedule";
 import { STrainings } from "./components/STrainings";
-import { SExercise } from "./components/SExercise";
-import { SSeries } from "./components/SSeries";
 import { RunTraining } from "./components/RunTraining";
 import { User } from "./components/User";
 import { Admin } from "./components/Admin";
@@ -25,20 +23,18 @@ export default class App extends Component {
     };
   }
 
-  isAuthenticated() {
-    fetch("api/user/check", {
+  isAuthenticated = async () => {
+    const check = await fetch("api/user/check", {
       method: "GET",
       mode: "cors",
       cache: "no-cache",
       credentials: "include",
-    }).then((response) => {
-      if (response.ok) {
-        this.setState({
-          isAuthenticated: true,
-        });
-      }
     });
-  }
+    const result = await check.json();
+    this.setState({
+      isAuthenticated: result.ok,
+    });
+  };
   handleLogout = () => {
     fetch("api/user/logout").then(() => {
       this.setState({
@@ -59,14 +55,19 @@ export default class App extends Component {
         isAuthenticated={this.state.isAuthenticated}
         handleLogout={this.handleLogout}
       >
-        <Route exact path="/" component={Home} />
+        <Route
+          exact
+          path="/"
+          render={(props) => <Home handleLogin={this.handleLogin} {...props} />}
+        />
         <Route path="/trening" exact component={Trainings} />
         <Route path="/cwiczenia" component={Exercises} />
         <Route path="/seria" component={Series} />
         <Route path="/harmonogram" exact component={Schedule} />
         <Route path="/harmonogram/:trainingId" exact component={STrainings} />
-        <Route path="/harmonogram/:trainingId/start" component={RunTraining} />
+        <Route path="/start/:trainingId" component={RunTraining} />
         <Route path="/konto" component={Account} />
+        <Route path="/prywatnosc" component={Account} />
         <Route
           path="/logowanie"
           render={(props) => <User handleLogin={this.handleLogin} {...props} />}
